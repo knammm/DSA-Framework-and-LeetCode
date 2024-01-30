8000967fc01faf4e4f2e93a38b2706121aa81e78
olkmphy <nam.nguyenolkmphy@hcmut.edu.vn>
2024-01-30 15:35:34 +0700

    Solving leetcode problem

---
+++ LeetCode Solving/Logic/singleNumber.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // XOR property: different => add, same => 0. 
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};

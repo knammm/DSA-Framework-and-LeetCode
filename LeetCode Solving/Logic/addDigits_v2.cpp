3485b3f141975c598cc752a7237bd552cac1f21e
olkmphy <nam.nguyenolkmphy@hcmut.edu.vn>
2024-02-03 17:46:59 +0700

    Solving LeetCode problem

---
+++ LeetCode Solving/Logic/addDigits_v2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        while(num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+        num = sum;
+        if(num >= 10){
+            return addDigits(num);
+        }
+        else{
+            return num;
+        }
+    }
+};
